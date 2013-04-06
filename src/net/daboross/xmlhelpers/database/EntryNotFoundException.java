f98ae7b3825461df5f90f2bc6da6234c134729d3
Dabo Ross <dabodialga@gmail.com>
2013-04-05 23:08:13 -0700

    Initial Commit

---
+++ src/net/daboross/xmlhelpers/database/EntryNotFoundException.java
@@ -0,0 +1,24 @@
+package net.daboross.xmlhelpers.database;
+
+/**
+ *
+ * @author daboross
+ */
+public class EntryNotFoundException extends Exception {
+
+    public EntryNotFoundException() {
+        super();
+    }
+
+    public EntryNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntryNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public EntryNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
